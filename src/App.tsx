66d0123f432bdd23bb2887c4811b73197fce7370
cannavaro944 <pedromiguellindo2005@gmail.com>
2025-03-17 15:31:03 +0000

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Mapa from "./components/Mapa";
+import Google from "./components/Google";
 
 const App: React.FC = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>🚍 Simulador de Autocarros - Braga</h1>
-      <Mapa />
+      <Google />
     </div>
   );
 };
---
+++ src/components/Google.tsx
@@ -0,0 +1,29 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import './Google.css';
+const Google = () => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: 'AIzaSyC25BF9tRi5zBubn6Uds0sHDjBMqkE1vHE',
+      })
+
+    return (
+
+<div className='map'>
+    {isLoaded ? (
+        <GoogleMap
+        mapContainerStyle={{width: '100%', height: '100%'}}
+        center={{
+            lat: 41.5454, 
+            lng: -8.4265,
+      }}
+      zoom={14}
+    ></GoogleMap>
+  ) : (
+    <></>
+)}
+</div>
+
+    );
+};
+
+export default Google;
