0440ff2ca4754306199552c3c0de6f43151cdccf
cannavaro944 <pedromiguellindo2005@gmail.com>
2025-03-20 21:23:10 +0000

    aladw

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Mapa from "./components/Mapa";
 import Google from "./components/Google";
-import Weather from "./components/Weather";
 
 const App: React.FC = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>🚍 Simulador de Autocarros - Braga</h1>
      <Google />
-      <Weather/>
-   
+      
     </div>
   );
 };
--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -1,9 +1,19 @@
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Image from "../assets/x.png";
+import Image1 from "../assets/locationarrow.png";
+
+
+
 import './Google.css';
+
 const Google = () => {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: 'AIzaSyC25BF9tRi5zBubn6Uds0sHDjBMqkE1vHE',
+        googleMapsApiKey: 'AIzaSyBiTWTQg2hgb56o8b2e69yVOFm7Rbx5Ymg',
       })
 
     return (
@@ -24,6 +34,42 @@ const Google = () => {
           <p>Carregando mapa...</p>
         )}
       </div>
+
+
+
+      {/*route*/}
+      <div className="absolute left-0 top-130 -full flex items-start p-4">
+      <Card className="p-4 rounded-lg mt-4 bg-white shadow-md w-full max-w-md">
+        <div className="space-y-4">
+          <div className="flex gap-4">
+            <Input type="text" placeholder="Origin" />
+            <Input type="text" placeholder="Destination" />
+          </div>
+          <div className="flex gap-4">
+            <Button className="bg-pink-500 hover:bg-pink-600 text-white">
+              Calculate Route
+            </Button>
+            <button
+              onClick={() => GoogleMap}
+              className="p-2 rounded-full bg-gray-200 hover:bg-gray-300"
+            >
+              
+            </button>
+          </div>
+          <div className="flex justify-between items-center">
+            <span>Distance:</span>
+            <span>Duration:</span>
+            <button
+              onClick={() => alert(123)}
+              className="p-2 rounded-full bg-gray-200 hover:bg-gray-300"
+            >
+
+            </button>
+          </div>
+        </div>
+      </Card>
+    </div>
+      
 
       {/* Painel de Informações ao lado */}
       <div className="info-panel">
